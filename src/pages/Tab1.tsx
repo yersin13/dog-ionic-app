d2a4c907206d89c8ada045f6c8f3db886384b57e
yersin13 <52936287+yersin13@users.noreply.github.com>
2020-12-02 23:44:21 -0500

    first commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonCard,
+  IonToolbar, useIonViewWillEnter,
+  IonInfiniteScroll, IonInfiniteScrollContent
+} from '@ionic/react';
+import React, {useState} from 'react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+
+  const [items, setItems] = useState<string[]>([]);
+
+  const [disableInfiniteScroll, setDisableInfiniteScroll] = useState<boolean>(false);
+
+  async function fetchData() {
+    const url: string = 'https://dog.ceo/api/breeds/image/random/10';
+
+    const res: Response = await fetch(url);
+    res
+        .json()
+        .then(async (res) => {
+          if (res && res.message && res.message.length > 0) {
+            setItems([...items, ...res.message]);
+            setDisableInfiniteScroll(res.message.length < 10);
+          } else {
+            setDisableInfiniteScroll(true);
+          }
+        })
+        .catch(err => console.error(err));
+  }
+
+  useIonViewWillEnter(async () => {
+    await fetchData();
+  });
+
+  async function searchNext($event: CustomEvent<void>) {
+    await fetchData();
+
+    ($event.target as HTMLIonInfiniteScrollElement).complete();
+  }
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle>Tab One</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+        <IonContent>
+          {items.map((item: string, i: number) => {
+            return <IonCard key={`${i}`}><img src={item}/></IonCard>
+          })}
+
+          <IonInfiniteScroll threshold="100px" disabled={disableInfiniteScroll}
+                             onIonInfinite={(e: CustomEvent<void>) => searchNext(e)}>
+            <IonInfiniteScrollContent
+                loadingText="Loading more good doggos...">
+            </IonInfiniteScrollContent>
+          </IonInfiniteScroll>
+        </IonContent>
+      </IonPage>
   );
 };
 
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent } from '@ionic/react';
 
-const Tab3: React.FC = () => {
+const Tab3Page: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Tab Three</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
+      <IonContent />
     </IonPage>
   );
 };
 
-export default Tab3;
+export default Tab3Page;
